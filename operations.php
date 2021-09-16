8bed727c3cc7c4fe6f22b137b501fbdc55868839
vikas chauhan <vikas.chauhan@successive.tech>
2021-09-16 14:52:25 +0530

    maths

---
+++ operations.php
@@ -0,0 +1,19 @@
+<?php
+function add($x, $y){
+	return $x + $y;
+}
+function subtract($x, $y){
+	return $x - $y;
+}
+function multiply($x, $y){
+	return $x * $y;
+}
+function divide($x, $y){
+	return $x / $y;
+}
+echo add(1,2).PHP_EOL;
+echo subtract(1,2).PHP_EOL;
+echo multiply(1,2).PHP_EOL;
+echo divide(1,2).PHP_EOL;
+?>
+
